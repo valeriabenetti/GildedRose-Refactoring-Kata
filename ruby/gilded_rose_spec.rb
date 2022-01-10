fe8fb92f8111575d6e6de2d2a0efde5d5b9703d7
valeriabenetti <vbenetti46@gmail.com>
2022-01-10 13:29:21 -0500

    Added more Tests to the Spec

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -16,7 +16,7 @@ describe GildedRose do
       expect(items[0].name).to eq expected_name
     end
 
-    it "change sell in value" do
+    it "changes sell in value" do
       #arrange
       sell_in_init = 4
       expected_result = sell_in_init - 1
@@ -82,7 +82,7 @@ describe GildedRose do
       expect(items[0].sell_in).to eq 0
     end
 
-    it "quality of an item is never more than 50" do
+    it "checks quality of an item is never more than 50" do
       #arrange
       initial_quality = 50
       expected_quality = initial_quality
@@ -95,39 +95,96 @@ describe GildedRose do
       expect(items[0].quality).to eq expected_quality
     end
 
-    # TODO: Sulfuras
+    it "checks that Sulfuras quality is always 80" do
+      #arrange
+      initial_quality = 80
+      expected_quality = initial_quality
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 1, initial_quality)]
+
+      #act
+      GildedRose.new(items).update_quality()
+
+      #assert
+      expect(items[0].quality).to eq expected_quality
+    end
+
+    context "checks that Backstage Passes" do
+
+      it "increases in quality as its sell_in value is > 10" do
+        #arrange
+        initial_quality = 20
+        initial_sell_in = 12
+        expected_quality = initial_quality + 1
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", initial_sell_in, initial_quality)]
+
+        #act
+        GildedRose.new(items).update_quality()
+
+        #assert
+        expect(items[0].quality).to eq expected_quality
+      end
+
+      it "increases in quality by 2 as its sell_in value is <= 10" do
+        #arrange
+        initial_quality = 20
+        initial_sell_in = 10
+        expected_quality = initial_quality + 2
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", initial_sell_in, initial_quality)]
+
+        #act
+        GildedRose.new(items).update_quality()
+
+        #assert
+        expect(items[0].quality).to eq expected_quality
+      end
+
+      it "increases in quality by 3 as its sell_in value is <= 5" do
+        #arrange
+        initial_quality = 20
+        initial_sell_in = 5
+        expected_quality = initial_quality + 3
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", initial_sell_in, initial_quality)]
+
+        #act
+        GildedRose.new(items).update_quality()
+
+        #assert
+        expect(items[0].quality).to eq expected_quality
+      end
+
+      it "quality drops to 0 after the sell_in date" do
+        #arrange
+        initial_quality = 1000
+        initial_sell_in = 0
+        expected_quality = 0
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", initial_sell_in, initial_quality)]
+
+        #act
+        GildedRose.new(items).update_quality()
+
+        #assert
+        expect(items[0].quality).to eq expected_quality
+      end
+
+    end
+
+    it "check conjured items quality degrades twice as fast" do
+      #arrange
+      initial_quality = 20
+      initial_sell_in = 10
+      expected_quality = initial_quality - 2
+      items = [Item.new("", initial_sell_in, initial_quality)]
+
+      # act
+
+      # assert
+
+    end
+    # TODO: Finish Conjured Spec Test
+    #       Refactor Gilded Rose File
     #       Code Snippets
     #       Describe it block naming conventions
     #       Keyboard shortcuts for expanding and collapsing code blocks
   end
 
 end
-
-# - All items have a SellIn value which denotes the number of days we have to sell the item
-# - All items have a Quality value which denotes how valuable the item is
-# - At the end of each day our system lowers both values for every item
-#
-# Pretty simple, right? Well this is where it gets interesting:
-#
-# - Once the sell by date has passed, Quality degrades twice as fast
-# - The Quality of an item is never negative
-# - "Aged Brie" actually increases in Quality the older it gets
-# - The Quality of an item is never more than 50
-# - "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
-#
-# - "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
-# Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
-# Quality drops to 0 after the concert
-#
-# We have recently signed a supplier of conjured items. This requires an update to our system:
-#
-# - "Conjured" items degrade in Quality twice as fast as normal items
-#
-# Feel free to make any changes to the UpdateQuality method and add any new code as long as everything
-# still works correctly. However, do not alter the Item class or Items property as those belong to the
-# goblin in the corner who will insta-rage and one-shot you as he doesn't believe in shared code
-# ownership (you can make the UpdateQuality method and Items property static if you like, we'll cover
-# for you).
-#
-# Just for clarification, an item can never have its Quality increase above 50, however "Sulfuras" is a
-# legendary item and as such its Quality is 80 and it never alters.
